cd8f08740a3e0e30713e273c48aa13a323f30b38
SuetamId <mmcc.silveira@gmail.com>
2022-11-24 23:08:25 -0300

    add metodo buscaId e atualizar

--- spring_spring/src/main/java/br/com/springboot/spring/controller/UsuarioController.java
+++ spring_spring/src/main/java/br/com/springboot/spring/controller/UsuarioController.java
@@ -6,15 +6,15 @@ import java.util.List;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.DeleteMapping;
 import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.PutMapping;
 import org.springframework.web.bind.annotation.RequestBody;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RequestMethod;
+import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.ResponseBody;
-import org.springframework.web.bind.annotation.ResponseStatus;
 import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.service.annotation.PutExchange;
 
 import br.com.springboot.spring.model.Usuario;
 import br.com.springboot.spring.repository.UsuarioRepository;
@@ -24,34 +24,9 @@ public class UsuarioController {
 
 	@Autowired /* IC/CD ou CDI - Injeção de dependencia */
 	private UsuarioRepository usuarioRepository;
-	
-	
-	/**
-	 *
-	 * @param name the name to greet
-	 * @return greeting text
-	 */
 
-	@RequestMapping(value = "teste/{nome}", method = RequestMethod.GET)
-	@ResponseStatus(HttpStatus.OK)
-	
-	public String retornaOlaMundo(@PathVariable String nome) {
-		
-		Usuario usuario2= new Usuario();
-		usuario2.setNome(nome);
-		
-		Usuario usuario = new Usuario();
-		usuario.setNome(nome);
-		
-		usuarioRepository.save(usuario);/* grava no banco de dados */
-
-		return "Ola mundo " + nome;
-}
-	
-	
 	/*metodo mostrar usuarios cadastrados no banco*/
 	@GetMapping(value = "listatodos")
-	
 	@ResponseBody /*retorna JSON*/
 	public ResponseEntity<List<Usuario>>listar(){
 		List<Usuario> usuarios = usuarioRepository.findAll();/**faz a consulta no banco*/
@@ -69,5 +44,39 @@ public class UsuarioController {
 		return new ResponseEntity<Usuario>(user, HttpStatus.CREATED);
 	}
 	
+	/*metodo deletar dados no banco*/
+	@DeleteMapping(value = "delete")
+	@ResponseBody
+	public ResponseEntity<String> delete(@RequestParam Long iduser){
+		
+		usuarioRepository.deleteById(iduser);
+		
+		return new ResponseEntity<String>("User deletado com sucesso", HttpStatus.OK);
+	}
+	
+	/*metodo buscar pelo id dados no banco*/
+	@GetMapping(value = "buscaruserid")
+	@ResponseBody
+	public ResponseEntity<Usuario> buscaruserid(@RequestParam(name = "iduser") Long iduser){
+		
+		Usuario	usuario = usuarioRepository.findById(iduser).get();
+		
+		return new ResponseEntity<Usuario>(usuario, HttpStatus.OK);
+	}
+	
+	
+	/*metodo atualizar dados no banco*/
+	@PutMapping(value = "atualizar")
+	@ResponseBody
+	public ResponseEntity<?> atualzar(@RequestBody Usuario usuario){
+		
+		
+		if (usuario.getId()== null) {
+			return new ResponseEntity<String>("Usuario não encontrado", HttpStatus.OK);
+		}
+		Usuario	user = usuarioRepository.saveAndFlush(usuario);
+		
+		return new ResponseEntity<Usuario>(user, HttpStatus.OK);
+	}
 	
 }
